bf2bbb534eba175b813586f16bdc330e23a15ae8
yanzhen <sdyzsdut@163.com>
2021-01-25 18:19:32 +0800

    capture

--- YZMetalKit/YZOutputCapture.h
+++ YZMetalKit/YZOutputCapture.h
@@ -6,8 +6,59 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIView.h>
 
+typedef NS_ENUM(NSInteger, YZOutputFillMode) {
+    YZOutputFillModeScaleToFill,       // Same as UIViewContentModeScaleToFill
+    YZOutputFillModeScaleAspectFit,    // Same as UIViewContentModeScaleAspectFit
+    YZOutputFillModeScaleAspectFill,   // Same as UIViewContentModeScaleAspectFill
+};
+
+@protocol YZOutputCaptureDelegate;
 @interface YZOutputCapture : NSObject
+/**
+ set out size, front is YES
+ */
+- (instancetype)initWithSize:(CGSize)size;
+- (instancetype)initWithSize:(CGSize)size front:(BOOL)front;
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)new NS_UNAVAILABLE;
+
+@property (nonatomic, weak) id<YZOutputCaptureDelegate> delegate;
+
+/** video player */
+@property (nonatomic, strong) UIView *player;
+/** video player fillMode*/
+@property (nonatomic) YZOutputFillMode fillMode;
+
+/** output size */
+@property (nonatomic, assign) CGSize size;
+/** YES: AVCaptureDevicePositionFront, NO: AVCaptureDevicePositionBack */
+@property (nonatomic, assign) BOOL front;
+/** default is 15, you can set (0,60), some device not support 60 */
+@property (nonatomic, assign) int32_t frameRate;
+/**
+ default is YES.
+ Only use for AVCaptureDevicePositionFront
+ */
+@property (nonatomic) BOOL videoMirrored;
+
+/** start video capture */
+- (void)startRunning;
+/** stop video capture */
+- (void)stopRunning;
 
 @end
+
+
+@protocol YZOutputCaptureDelegate <NSObject>
+
+@optional
+- (void)videoCapture:(YZOutputCapture *)videoCapture outputPixelBuffer:(CVPixelBufferRef)pixelBuffer;
+- (void)videoCapture:(YZOutputCapture *)videoCapture decodePixelBuffer:(CVPixelBufferRef)pixelBuffer;
+- (void)videoCapture:(YZOutputCapture *)videoCapture dropFrames:(int)frames;
+
+@end
+
 
--- YZMetalLib/OutputViewController.m
+++ YZMetalLib/OutputViewController.m
@@ -6,9 +6,13 @@
 //
 
 #import "OutputViewController.h"
+#import <YZMetalKit/YZOutputCapture.h>
 
 @interface OutputViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *palyer;
+@property (nonatomic, strong) CIContext *context;
 
+@property (nonatomic, strong) YZOutputCapture *capture;
 @end
 
 @implementation OutputViewController
@@ -17,8 +21,41 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"output";
+    _context = [CIContext contextWithOptions:nil];
+    
+}
+
+- (IBAction)backT:(UIButton *)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)switchCamera:(id)sender {
+    
 }
 
 
+- (void)showPixelBuffer:(CVPixelBufferRef)pixel {
+    CVPixelBufferRetain(pixel);
+    CIImage *ciImage = [CIImage imageWithCVImageBuffer:pixel];
+    size_t width = CVPixelBufferGetWidth(pixel);
+    size_t height = CVPixelBufferGetHeight(pixel);
+//    NSLog(@"XX___%d:%d", width, height);
+    CGImageRef videoImageRef = [_context createCGImage:ciImage fromRect:CGRectMake(0, 0, width, height)];
+    UIImage *image = [UIImage imageWithCGImage:videoImageRef];
+    CGImageRelease(videoImageRef);
+    CVPixelBufferRelease(pixel);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.palyer.image = image;
+    });
+}
+#pragma mark - system
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
 
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
 @end
