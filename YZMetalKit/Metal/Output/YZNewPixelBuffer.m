4861b440bef363270ebb5f3fa391c7e302fa9556
yanzhen <sdyzsdut@163.com>
2021-02-03 16:37:46 +0800

    new output

--- YZMetalKit/Metal/Output/YZNewPixelBuffer.m
+++ YZMetalKit/Metal/Output/YZNewPixelBuffer.m
@@ -6,8 +6,14 @@
 //
 
 #import "YZNewPixelBuffer.h"
+#import <CoreVideo/CVMetalTextureCache.h>
+#import "YZMetalOrientation.h"
+#import "YZMetalDevice.h"
+#import "YZShaderTypes.h"
 
 @interface YZNewPixelBuffer ()
+@property (nonatomic, assign) CVMetalTextureCacheRef textureCache;
+@property (nonatomic, strong) id<MTLTexture> texture;
 @property (nonatomic) CGSize size;
 @end
 
@@ -20,32 +26,52 @@
         CVPixelBufferRelease(_pixelBuffer);
         _pixelBuffer = nil;
     }
+    
+    if (_textureCache) {
+        CVMetalTextureCacheFlush(_textureCache, 0);
+        CFRelease(_textureCache);
+    }
 }
 
 - (instancetype)init
 {
     self = [super init];
     if (self) {
-        
+        CVMetalTextureCacheCreate(kCFAllocatorDefault, NULL, YZMetalDevice.defaultDevice.device, NULL, &_textureCache);
     }
     return self;
 }
 
-//输出默认分辨率
 -(void)newTextureAvailable:(id<MTLTexture>)texture commandBuffer:(id<MTLCommandBuffer>)commandBuffer {
-    [commandBuffer waitUntilCompleted];
-    [self dealTextureSize:texture];
-    //[self dealPixelBuffer:texture];
+    [self newDealTextureSize:texture];
     if (!_pixelBuffer) { return; }
     
-    CVPixelBufferLockBaseAddress(_pixelBuffer, 0);
-    void *address = CVPixelBufferGetBaseAddress(_pixelBuffer);
-    if (address) {
-        size_t bytesPerRow = CVPixelBufferGetBytesPerRow(_pixelBuffer);
-        MTLRegion region = MTLRegionMake2D(0, 0, _size.width, _size.height);
-        [texture getBytes:address bytesPerRow:bytesPerRow fromRegion:region mipmapLevel:0];
+    [self nnnBuffer:texture];
+}
+
+- (void)nnnBuffer:(id<MTLTexture>)texture {
+    MTLRenderPassDescriptor *desc = [YZMetalDevice newRenderPassDescriptor:_texture];
+    id<MTLCommandBuffer> commandBuffer = [YZMetalDevice.defaultDevice commandBuffer];
+    id<MTLRenderCommandEncoder> encoder = [commandBuffer renderCommandEncoderWithDescriptor:desc];
+    if (!encoder) {
+        NSLog(@"YZVideoCamera render endcoder Fail");
+        return;
     }
-    CVPixelBufferUnlockBaseAddress(_pixelBuffer, 0);
+
+    [encoder setFrontFacingWinding:MTLWindingCounterClockwise];
+    [encoder setRenderPipelineState:self.pipelineState];
+    simd_float8 vertices = [YZMetalOrientation defaultVertices];
+    [encoder setVertexBytes:&vertices length:sizeof(simd_float8) atIndex:YZVertexIndexPosition];
+    
+    simd_float8 textureCoordinates = [YZMetalOrientation defaultTextureCoordinates];
+    [encoder setVertexBytes:&textureCoordinates length:sizeof(simd_float8) atIndex:YZVertexIndexTextureCoordinate];
+    [encoder setFragmentTexture:texture atIndex:YZFragmentTextureIndexNormal];
+    
+    [encoder drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:0 vertexCount:4];
+    [encoder endEncoding];
+    
+    [commandBuffer commit];
+    [commandBuffer waitUntilCompleted];
     
     if ([_delegate respondsToSelector:@selector(outputPixelBuffer:)]) {
         [_delegate outputPixelBuffer:_pixelBuffer];
@@ -53,9 +79,8 @@
     [super newTextureAvailable:texture commandBuffer:commandBuffer];
 }
 
-
 #pragma mark - output texture size
-- (void)dealTextureSize:(id<MTLTexture>)texture {
+- (void)newDealTextureSize:(id<MTLTexture>)texture {
     CGFloat width = texture.width;
     CGFloat height = texture.height;
     if (!CGSizeEqualToSize(_size, CGSizeMake(width, height))) {
@@ -65,13 +90,8 @@
         }
         _size = CGSizeMake(width, height);
     }
-    if (!_pixelBuffer) {
-        [self generatePixelBuffer];
-    }
-}
-
-#pragma mark  - private render
-- (void)generatePixelBuffer {
+    
+    if (_pixelBuffer) { return; }
     NSDictionary *pixelAttributes = @{(NSString *)kCVPixelBufferIOSurfacePropertiesKey:@{}};
     CVReturn result = CVPixelBufferCreate(kCFAllocatorDefault,
                                             _size.width,
@@ -81,6 +101,17 @@
                                             &_pixelBuffer);
     if (result != kCVReturnSuccess) {
         NSLog(@"YZNewPixelBuffer to create cvpixelbuffer %d", result);
+        return;
     }
+    
+    CVMetalTextureRef textureRef = NULL;
+    CVReturn status = CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault, _textureCache, _pixelBuffer, nil, MTLPixelFormatBGRA8Unorm, width, height, 0, &textureRef);
+    if (kCVReturnSuccess != status) {
+        return;
+    }
+    _texture = CVMetalTextureGetTexture(textureRef);
+    CFRelease(textureRef);
+    textureRef = NULL;
+    NSLog(@"bb009:%@:%@",_texture, self.pipelineState);
 }
 @end
