4e9d84d86a772561d6ee7c13ec3331e0a4977fa4
yanzhen <sdyzsdut@163.com>
2021-02-18 16:03:33 +0800

    filter capture

--- YZMetalKit/YZFilterCapture.h
+++ YZMetalKit/YZFilterCapture.h
@@ -6,11 +6,65 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIView.h>
 
-NS_ASSUME_NONNULL_BEGIN
+typedef NS_ENUM(NSInteger, YZFilterFillMode) {
+    YZFilterFillModeScaleToFill,       // Same as UIViewContentModeScaleToFill
+    YZFilterFillModeScaleAspectFit,    // Same as UIViewContentModeScaleAspectFit
+    YZFilterFillModeScaleAspectFill,   // Same as UIViewContentModeScaleAspectFill
+};
 
+@protocol YZFilterCaptureDelegate;
 @interface YZFilterCapture : NSObject
+/**
+ set out size, front is YES
+ */
+- (instancetype)initWithSize:(CGSize)size front:(BOOL)front;
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)new NS_UNAVAILABLE;
+
+@property (nonatomic, weak) id<YZFilterCaptureDelegate> delegate;
+
+/** video player */
+@property (nonatomic, strong) UIView *player;
+/** video player fillMode*/
+@property (nonatomic) YZFilterFillMode fillMode;
+
+/** output size */
+@property (nonatomic, assign) CGSize size;
+/** YES: AVCaptureDevicePositionFront, NO: AVCaptureDevicePositionBack */
+@property (nonatomic, assign) BOOL front;
+/** default is 15, you can set (0,60), some device not support 60 */
+@property (nonatomic, assign) int32_t frameRate;
+/**
+ default is YES.
+ Only use for AVCaptureDevicePositionFront
+ */
+@property (nonatomic) BOOL videoMirrored;
+
+/** default is YES */
+@property (nonatomic, assign) BOOL beautyEnable;
+/** default is 0.5 */
+@property (nonatomic, assign) float beautyLevel;
+/** default is 0.5 */
+@property (nonatomic, assign) float brightLevel;
+
+/** start video capture */
+- (void)startRunning;
+/** stop video capture */
+- (void)stopRunning;
+
+- (void)setWatermark:(UIImage *)image frame:(CGRect)frame;
 
 @end
 
-NS_ASSUME_NONNULL_END
+
+@protocol YZFilterCaptureDelegate <NSObject>
+
+@optional
+- (void)videoCapture:(YZFilterCapture *)videoCapture outputPixelBuffer:(CVPixelBufferRef)pixelBuffer;
+- (void)videoCapture:(YZFilterCapture *)videoCapture dropFrames:(int)frames;
+
+@end
+
--- YZMetalLib/Filters/FiltersViewController.m
+++ YZMetalLib/Filters/FiltersViewController.m
@@ -8,8 +8,13 @@
 #import "FiltersViewController.h"
 #import <YZMetalKit/YZMetalKit.h>
 
-@interface FiltersViewController ()
+@interface FiltersViewController ()<YZFilterCaptureDelegate>
+@property (weak, nonatomic) IBOutlet UIView *player;
+@property (weak, nonatomic) IBOutlet UIImageView *smallPlayer;
+
+
 @property (nonatomic, strong) YZFilterCapture *capture;
+@property (nonatomic, strong) CIContext *context;
 @end
 
 @implementation FiltersViewController
@@ -17,9 +22,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _context = [CIContext contextWithOptions:nil];
     
-    
+    _capture = [[YZFilterCapture alloc] initWithSize:CGSizeMake(480, 480) front:YES];
+    _capture.fillMode = YZFilterFillModeScaleAspectFit;
+    _capture.player = self.player;
+//    _videoCapture.fillMode = YZVideoFillModeScaleAspectFit;
+    _capture.delegate = self;
+    [_capture startRunning];
 }
 
+
+- (IBAction)back:(UIButton *)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)showPixelBuffer:(CVPixelBufferRef)pixel {
+    CVPixelBufferRetain(pixel);
+    CIImage *ciImage = [CIImage imageWithCVImageBuffer:pixel];
+    size_t width = CVPixelBufferGetWidth(pixel);
+    size_t height = CVPixelBufferGetHeight(pixel);
+    CGImageRef videoImageRef = [_context createCGImage:ciImage fromRect:CGRectMake(0, 0, width, height)];
+    UIImage *image = [UIImage imageWithCGImage:videoImageRef];
+    CGImageRelease(videoImageRef);
+    CVPixelBufferRelease(pixel);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        //self.smallPlayer.image = image;
+    });
+}
+#pragma mark - system
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
 
 @end
